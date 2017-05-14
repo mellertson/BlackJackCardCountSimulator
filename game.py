96bd0f23477f6fb02c73352fc868a5a6c3f67935
pellertson <parkerellertson34@gmail.com>
2017-05-13 18:48:45 -0700

    Created Game class & added count, trueCount, betAmmount, bankRoll, decks, and losses variables.

---
+++ game.py
@@ -0,0 +1,9 @@
+class Game(object):
+    __init__(self, gameNumber):
+        self.count = 0
+        self.trueCount = 0
+        self.betAmmount = 0.0
+        self.bankRoll = 500.0
+        self.decks = 6
+        self.wins = 0
+        self.losses = 0
