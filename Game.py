5d9280a4374c86370f8d1e27c7fea122444fa676
pellertson <parkerellertson34@gmail.com>
2017-05-13 22:17:59 -0600

    Completed init method. Added documentation.

--- Game.py
+++ Game.py
@@ -1,8 +1,24 @@
 import ConfigFileManager as cfm
 
 class Game(object):
-    # Test
-    __init__(self, gameNumber):
+    """Construct a new <code>Game</code> object.
+    
+    Attributes:
+        count: An integer describing the ratio between low cards (2-6) and
+            high cards (10-A).
+        trueCount: The count divided by the number of decks in play.
+        betAmmount: How much you want to bet. Minimum bet in casinos is usually
+            $2.00.
+        bankRoll: The ammount of money left in the game. Default ammount is
+            $500.00.
+        decks: The number of decks that have not been played yet.
+        wins: Number of times the player wins a hand.
+        losses: Number of times the player loses a hand.
+        ties: Number of times the player pushes a hand.
+        gameNumber: How many blackjack simulations were run. Used in making the
+            file name
+    """
+    def __init__(self, gameNumber):
         self.count = 0
         self.trueCount = 0
         self.betAmmount = 2.0
@@ -10,8 +26,8 @@ class Game(object):
         self.decks = 6
         self.wins = 0
         self.losses = 0
-        
-        cfm
-        
-    
+        self.ties = 0
+        self.gameNumber = gameNumber
+
+
 
