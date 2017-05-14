6ca5264488ac4633ec23d2a2418760bdc9d983cb
pellertson <parkerellertson34@gmail.com>
2017-05-13 19:05:17 -0700

    Made betAmmount default 2.0.

--- game.py
+++ game.py
@@ -2,7 +2,7 @@ class Game(object):
     __init__(self, gameNumber):
         self.count = 0
         self.trueCount = 0
-        self.betAmmount = 0.0
+        self.betAmmount = 2.0
         self.bankRoll = 500.0
         self.decks = 6
         self.wins = 0
