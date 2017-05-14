f0cbf760e06f7bac4b2a53e0f2076f0eed5465b2
Mike Ellertson <mdellertson@gmail.com>
2017-05-13 22:46:22 -0600

    Added View.main() method.

--- View.py
+++ View.py
@@ -1,3 +1,4 @@
 
 class View:
-    pass
+    def main(self):
+        pass
--- tests/test_View.py
+++ tests/test_View.py
@@ -5,5 +5,10 @@ class View_TestCase(unittest.TestCase):
     def test_view_class_exists(self):
         from View import View
 
+    def test_main_method_exists(self):
+        from View import View
+        view = View()
+        view.main()
+
 
 
